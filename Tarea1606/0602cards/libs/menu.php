cc5e88849a94b41649c0d306e9419d282a39d8e9
Nathalia Schneider <81602411+nathischneider03@users.noreply.github.com>
2022-06-22 13:35:46 -0400

    Delete Tarea1606 directory

--- Tarea1606/0602cards/libs/menu.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar navbar-light bg-light">
-  <p class="form-inline">
-    <a class="btn btn-outline-success" href="/Tarea1606/0602cards/ciudades/" >Ciudades</a>
-    <a class="btn btn-outline-success" href="/Tarea1606/0602cards/personas/"  >Personas</a>
-  </p>
-</nav>
